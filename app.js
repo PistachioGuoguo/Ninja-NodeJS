23e1053eb97aaec23a893d9d8387180cbfe1201b
Pistachio Guoguo <sangzizhou@gmail.com>
2021-03-07 23:14:33 -0600

    set MongoDB

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const app = express();
+const Blog = require('./models/blog');
+const mongoose = require('mongoose');
 
-app.listen(3000);
+
 
 app.set('view engine', 'ejs'); // under views folder
-// app.set('views','myviews'); // set diff folder of 'views'
+
+app.use(express.static('public'));
+
+// connect to db
+
+const dbURI = 'mongodb+srv://sam:1111@node-tutorial.u1k3h.mongodb.net/node-tuts?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then((result) => app.listen(3000))
+.catch((err) => console.log(err));
+
 
 // get 
 app.get('/', (req, res) => {
 
-    const blogs = [
-        {title: 'I am happy today', snippet: 'Happy'},
-        {title: 'Good luck in future', snippet: 'You got it!'},
-        {title: 'Pistachio is cute', snippet: 'Cute pistachios'},
-    ]
-
-    res.render('index', {title: 'Home', blogs});
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
@@ -25,6 +31,18 @@ app.get('/about', (req, res) => {
 app.get('/about-us', (req, res) => {
     res.redirect('about');  
 });
+
+
+// blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find()
+        .then((result) => {
+            res.render('index', {title : 'All Blogs', blogs: result})
+        })
+        .catch( err => {
+            console.log(arr);
+        })
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', {title: 'Home'});
