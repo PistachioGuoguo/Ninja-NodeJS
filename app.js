57889b8a709dc5a25f0b1efbab5aed356d6cc889
Pistachio Guoguo <sangzizhou@gmail.com>
2021-03-08 08:09:30 -0600

    add POST request

--- app.js
+++ app.js
@@ -4,23 +4,22 @@ const Blog = require('./models/blog');
 const mongoose = require('mongoose');
 
 
-
 app.set('view engine', 'ejs'); // under views folder
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true})); // pass url data to handler (POST)
 
 // connect to db
 
 const dbURI = 'mongodb+srv://sam:1111@node-tutorial.u1k3h.mongodb.net/node-tuts?retryWrites=true&w=majority';
 
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-.then((result) => app.listen(3000))
-.catch((err) => console.log(err));
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 
 // get 
 app.get('/', (req, res) => {
-
     res.redirect('/blogs');
 });
 
@@ -35,17 +34,32 @@ app.get('/about-us', (req, res) => {
 
 // blog routes
 app.get('/blogs', (req, res) => {
-    Blog.find()
+    Blog.find().sort({createdAt : -1}) // sort by desc createTime
         .then((result) => {
+            console.log(result);
             res.render('index', {title : 'All Blogs', blogs: result})
         })
         .catch( err => {
             console.log(arr);
         })
+});
+
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then( (result) => {
+            res.redirect('/blogs');
+        })
+        .catch( err => {
+            console.log(err);
+        })
 })
 
+
 app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Home'});
+    res.render('create', {title: 'Create a new Blog'});
 });
 
 app.use((req, res) => {
